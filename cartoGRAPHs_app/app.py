c6cfca108307f6aa0d4053a9e0669880b955c171
Meler Daniel <a11807113@unet.univie.ac.at>
2024-12-16 13:29:47 +0100

    Adjusted server_run command to map to localhost so that the app is correctly displayed.

--- cartoGRAPHs_app/app.py
+++ cartoGRAPHs_app/app.py
@@ -947,7 +947,7 @@ def download_xgmml():
 server = app.server
 if __name__ == '__main__':
     #print('we are in --main__')
-    app.run_server(debug=True,
+    app.run_server(host='0.0.0.0', port=8050,debug=True,
                    use_reloader=False)
 
 
